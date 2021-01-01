f69bab0a3d8ce36aca75ea4e2d92c924e7650c96
siansiong97 <56067813+siansiong97@users.noreply.github.com>
2021-01-01 15:21:14 +0800

    hi

--- resources/js/common/config.js
+++ resources/js/common/config.js
@@ -1,6 +1,6 @@
 export let commonConfig = {
     // API_URL: 'http://172.104.45.205/api',
-    API_URL: 'http://localhost:8000',
+    API_URL: 'http://139.162.41.95/api',
     client_id : "2",
     client_secret : "OSvcRrEQypndMgeuUCEi4C0I17DA5a8aoEb4nrhA",
     google_maps_api_key : "AIzaSyBO1x-TEImkPZ0Owjl3J5daa706EFVw1kA",
