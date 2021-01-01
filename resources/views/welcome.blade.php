9b0e515f84c1f10b677a2061bf2edf5dd2943cdb
siansiong97 <56067813+siansiong97@users.noreply.github.com>
2021-01-01 14:32:30 +0800

    b

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -16,7 +16,7 @@
     <body>
         <div id="app">
         
-        <p>testtttt</p>
+        
             <router-view :key="$route.fullPath">
             </router-view>
             <vue-progress-bar></vue-progress-bar>
@@ -27,7 +27,6 @@
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-        <!-- <script src="http://139.162.41.59/js/app.js"></script> -->
-        <script src="{{mix('js/app.js')}}"></script>
+        <script src="{{ mix('js/app.js') }}"></script>
     </body>
 </html>
