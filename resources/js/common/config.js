32da2dec958a2dd889478df1cef106a3626363ef
siansiong97 <56067813+siansiong97@users.noreply.github.com>
2021-07-06 12:56:02 +0800

    config

--- resources/js/common/config.js
+++ resources/js/common/config.js
@@ -1,11 +1,11 @@
 export let commonConfig = {
     //live
-    // API_URL: 'http://139.162.41.95/api',
-    // client_secret : "OSvcRrEQypndMgeuUCEi4C0I17DA5a8aoEb4nrhA",
+    API_URL: 'http://139.162.41.95/api',
+    client_secret : "OSvcRrEQypndMgeuUCEi4C0I17DA5a8aoEb4nrhA",
     client_id : "2",
     //develop
-    API_URL: 'http://localhost:8000/api',
-    client_secret : "UHDIwCfH3efYLfpoPp3mBHPX0Y8PKvu7OJ9Josvl",
+    // API_URL: 'http://localhost:8000/api',
+    // client_secret : "UHDIwCfH3efYLfpoPp3mBHPX0Y8PKvu7OJ9Josvl",
     google_maps_api_key : "8WnW78jHuT0ZynxzuOa5BA32Sc0hKaZIRqZ2cxDm",
     cloudinary_cloud_name : "dwslzbgaa",
 };
