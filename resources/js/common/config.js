d10953dc9fa72d58dc8bdedb2912c11df2e81b16
siansiong97 <56067813+siansiong97@users.noreply.github.com>
2021-08-24 00:46:10 +0800

    merge

--- resources/js/common/config.js
+++ resources/js/common/config.js
@@ -1,12 +1,12 @@
 export let commonConfig = {
     //live
     // API_URL: 'http://139.162.41.95/api',
-    // API_URL: 'http://antzmanagement.com/api',
-    // client_secret : "OSvcRrEQypndMgeuUCEi4C0I17DA5a8aoEb4nrhA",
+    API_URL: 'http://antzmanagement.com/api',
+    client_secret : "OSvcRrEQypndMgeuUCEi4C0I17DA5a8aoEb4nrhA",
     client_id : "2",
     //develop
-    API_URL: 'http://localhost:8000/api',
-    client_secret : "zyPiIM80AX0tclAimp0iuTJ1Z0tEcGqhJs9LJ7bn",
+    // API_URL: 'http://localhost:8000/api',
+    // client_secret : "zyPiIM80AX0tclAimp0iuTJ1Z0tEcGqhJs9LJ7bn",
     google_maps_api_key : "8WnW78jHuT0ZynxzuOa5BA32Sc0hKaZIRqZ2cxDm",
     cloudinary_cloud_name : "dwslzbgaa",
 };
